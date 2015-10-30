1991578760d46774ce375123c7301409b206626e
RichardSZ <richardsz@foxmail.com>
2015-10-30 12:06:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-# _*_ coding:utf-8 _*_
+# _*_ coding: utf-8 _*_
 # Quick Python Script Expalanation for Programmers
 # 给程序员的超快速py脚本解说
+-*- coding: utf-8 -*-
 import os
 
 def main():
     print 'Hello world!'
 
-    print "This is Alice's greeting."
-    print 'This is Bob\'s greeting.'
+    print "这是Alice的问候."
+    print '这是Bob\的问候.'
 
     foo(5,10)
 
     print '=' * 10
-    print 'Current working directory is '+os.getcwd()
+    print '这将直接执行'+os.getcwd()
 
     counter = 0
     counter += 1
 
-    food = ['apples','oranges','cats']
+    food = ['苹果','杏子','李子','梨']
     for i in food:
-        print 'I like to eat '+i
+        print '俺就爱整只:'+i
 
-    print 'count to ten:'
+    print '数到10'
     for i in range(10):
         print i
 
 def foo(param1,secondparam):
     res = param1 + secondparam
-    print '%s plus %s equal %s' %(param1,secondparam,res)
+    print '%s 加 %s 等于 %s' %(param1,secondparam,res)
     if res < 50:
         print 'foo'
     elif(res>=50) and ((param1==42) or (secondparam==24)):
